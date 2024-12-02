1905f852a9f45d87dda92b94a744a29874f88ceb
RickEvry <rickevry@gmail.com>
2024-12-02 07:41:15 +0100

    work

--- print2.js
+++ print2.js
@@ -23,7 +23,7 @@ const darkness = "^MD15";
 const changeFont = "^CF00,40";
 const fieldOrigin = (x,y) => (`^FO${x},${y}`);
 const printText = (x,y,s) => (`^FO${x},${y}^FD${s}^FS`) 
-const printQRCode = (x, y, url) => (`^FO${x},${y}^BQ,2,7^FD${url}^FS`);
+const printQRCode = (x, y, url) => (`^FO${x},${y}^BQN,2,7,M,7^FDQA,${url}^FS`);
 const end = "^XZ";
 
 let zpl = 
@@ -33,22 +33,24 @@ let zpl =
     darkness +
     changeFont + 
 
-    printText(70,320,"320") + 
+    printText(70,322,"Q2024041") + 
 
-    printText(260,406,"406") +
-    printText(70,406,"406") +
-    printText(70,488,"488") +
+    printText(260,408,"R03113412") +
+    printText(70,408,"HNE023-12") +
+    printText(70,490,"Norra hallen") +
 
-    printText(260,574,"574") +
-    printText(260,618,"AAA") +
 
-    printText(70,574,"574") +
+    printText(260,576,"3.500 KG") +
+    printText(70,576,"G034") +
     
+    printText(260,620,"5 Pers") +
+
     //printText(53,710,"710") +
     
     printText(640,686,"2025 / 01") +
 
-    printQRCode(594, 96, "http://www.qvalify.se/id=abced24353&ejhfjewhrewr") +
+    //printQRCode(594, 96, "http://www.qvalify.se/id=abced24353&ejhfjewhrewr") +
+    printQRCode(606, 115, "http://www.qvalify.se/id=abced24353&ejhfjewhrewr") +
 
     end;
 
